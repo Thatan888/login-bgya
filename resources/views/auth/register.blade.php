8109fdcd9323e560beef2af8a44690a988027c08
Yonatan <bibianogonzalesyonatanalexis@gmail.com>
2024-06-05 13:14:56 -0600

    Login Funcionando

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,6 +9,15 @@
         <h1 class="h3 mb-3 fw-normal">Register</h1>
 
         <div class="form-group form-floating mb-3">
+            <input type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="Name"
+                required="required" autofocus>
+            <label for="floatingName">Name</label>
+            @if ($errors->has('username'))
+                <span class="text-danger text-left">{{ $errors->first('name') }}</span>
+            @endif
+        </div>
+
+        <div class="form-group form-floating mb-3">
             <input type="email" class="form-control" name="email" value="{{ old('email') }}"
                 placeholder="name@example.com" required="required" autofocus>
             <label for="floatingEmail">Email address</label>
@@ -45,7 +54,7 @@
         </div>
 
         <button class="w-100 btn btn-lg btn-primary" type="submit">Register</button>
-        <a href="{{ route('home.index') }}" class="btn btn-secondary">Back</a>
+        <a href="{{ route('home.index') }}" class="w-100 btn btn-lg mt-2 btn-secondary">Back</a>
         
         @include('auth.partials.copy')
     </form>
